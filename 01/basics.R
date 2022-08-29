e4695e0e261e98c97c803dfe252c845160a3f92a
bhedrick <brooke.t.hedrick@gmail.com>
2022-08-29 17:38:48 -0500

    Continued work on basics.R

--- 01/basics.R
+++ 01/basics.R
@@ -84,7 +84,61 @@ x[3]
 
 ### Exercise
 # 1 . Create the object "myobject" and assign the values 1:10 # in at least 3 different ways
+myobject = 1:10
+myobject = c(1:10)
+myobject = c(1,2,3,4,5,6,7,8,9,10)
+myobject <- (1:10)
+assign("myobject", 1:10)
+
 # 2. Get the sum of your object
+sum(myobject)
+
 # 3. Create the following vector by using the paste function "R is great 4 and I will love it" "R is great 7 and I will love it" "R is great 45 and I will love it"
+paste("R is great", c(4, 7, 45), "and I will love it")
+
 # 4. vector of 1,2,3 : repeat the vector to get 11 x 1, 10 x 2, and 10 x 3
+# this means 11 ones, 10 twos, and 10 threes
+x = rep(1:3, length = 31); x
+
 # 5. What is the value of this vector on position 7
+x[7]
+
+
+### Working with data.frames
+?airmiles
+airmiles
+head(airmiles)
+tail(airmiles)
+
+summary(mtcars)
+plot(mtcars)
+hist(airmiles)
+
+head(mtcars)
+sum(mtcars$wt)
+
+attach(mtcars)
+sum(wt)
+detach(mtcars)
+sum(wt)
+
+mtcars[2,6]
+mtcars[c(2,5,8),6]
+
+### Graphs in R Base
+# 3 main data viz systems: # ggplot2, lattice and R Base
+
+x=5:7
+y=8:10
+plot(x,y) # scatter plot (x,y)
+
+plot(lynx) # time series plot
+
+# title, color, title color, title magnification (font size)
+plot(lynx, main="Lynx Trappings", col="red", col.main=52, cex.main=1.5)
+
+# labels
+plot(lynx, ylab="Lynx Trappings", xlab="")
+
+# label orientation
+plot(lynx, ylab="Lynx Trappings", xlab="", las=2)
